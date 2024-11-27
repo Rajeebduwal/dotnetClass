ec1b0f67736657f9e8badd8c1abd22ec79c699d5
Rajib Duwal <duwalrajib8@gmail.com>
2024-11-27 12:29:20 +0545

    change

---
+++ ConsoleApp1/Crudeoperation.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace DatabaseExmaple
+{
+    class CrudeOperation
+    {
+        public void createTable()
+        {
+            try
+            {
+                //1.create connection string
+                string cs = "Data Source=DESKTOP-1GSF0CC;Integrated Security=True;Connect Timeout=30;";
+                //server name
+
+                //2.create database
+                SqlConnection conn = new SqlConnection(cs);
+
+                if (conn.State == ConnectionState.Open)
+                {
+                    Console.WriteLine("connected with server");
+                }
+                conn.Open();//connection secured
+                Console.WriteLine("connected with server");
+
+                string tblquery = "create table tbl_reg (id int primary key,username varchar(50), password varchar(50),repassword varchar(50)," + "gender varchar(50),faculty varchar(50))";
+                //executing query
+                SqlCommand sc = new SqlCommand(tblquery, conn);
+                sc.ExecuteNonQuery();//for create insert update and others
+                Console.WriteLine("table created");
+                Console.ReadLine();
+                conn.Close();//closing connection
+            }
+            catch (Exception s)
+            {
+                Console.WriteLine(s);
+            }
+        }
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;//tp connect to SQl and for sql lang
+//to connect with the eternal datasource like sql server,xml file,sql client
+//packet should be imported
+//steps to connect with database
+//1.create connection string that have all the parameter required to connect with database
+//2.
+
+namespace DatabaseExmaple
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CrudeOperation co = new CrudeOperation();
+            co.createTable();
+        }
+    }
+}
