0caa842a62d459f961d472c2d780fb8b8d214754
Rajib Duwal <duwalrajib8@gmail.com>
2024-11-28 14:28:52 +0545

    update

--- ConsoleApp1/Crudeoperation.cs
+++ ConsoleApp1/Crudeoperation.cs
@@ -4,38 +4,106 @@ using System.Data.SqlClient;
 
 namespace DatabaseExmaple
 {
-    class CrudeOperation
+    class CrudOperation
     {
-        public void createTable()
+        public void createTable() 
         {
             try
             {
-                //1.create connection string
-                string cs = "Data Source=DESKTOP-1GSF0CC;Integrated Security=True;Connect Timeout=30;";
-                //server name
+                //1. create connection string
+                string cs = "Data Source=DESKTOP-1GSF0CC;Initial Catalog=db_nccs; Integrated Security=true;Trusted_Connection = True";
+                // server name                                          database name           windowm authentication
 
-                //2.create database
+                //2. creating connection
                 SqlConnection conn = new SqlConnection(cs);
-
+                
                 if (conn.State == ConnectionState.Open)
                 {
-                    Console.WriteLine("connected with server");
+                    Console.WriteLine("Connected with server");
                 }
-                conn.Open();//connection secured
-                Console.WriteLine("connected with server");
+                conn.Open();// connection secured
+                Console.WriteLine("connected");
 
-                string tblquery = "create table tbl_reg (id int primary key,username varchar(50), password varchar(50),repassword varchar(50)," + "gender varchar(50),faculty varchar(50))";
-                //executing query
-                SqlCommand sc = new SqlCommand(tblquery, conn);
-                sc.ExecuteNonQuery();//for create insert update and others
+                string tblquery = "create table tbl_reg (id int primary key," + "username varchar(50), password varchar(50), repassword varchar(50)," +
+                                "gender varchar(50), faculty varchar(50))";
+                // executing query
+                SqlCommand sc= new SqlCommand(tblquery, conn);
+                sc.ExecuteNonQuery();// for create insert update and delete
                 Console.WriteLine("table created");
-                Console.ReadLine();
-                conn.Close();//closing connection
+                conn.Close();// closing connection
             }
             catch (Exception s)
             {
                 Console.WriteLine(s);
             }
         }
+        // inserting data into table
+        public void insertData()
+        {
+            try
+            {
+                string cs = "Data Source=DESKTOP-1GSF0CC;Initial Catalog=db_nccs; Integrated Security=true;Trusted_Connection = True";
+                SqlConnection conn = new SqlConnection(cs);
+                conn.Open();
+                // writing query for inserting data
+                //string insquery = "insert into tbl_reg values(1, 'pukar', 'abcd', 'abcd', 'male', 'csit')";
+                Console.WriteLine("enter your id");
+                string id= Console.ReadLine();
+                Console.WriteLine("Enter Username");
+                string uname=Console.ReadLine();
+                Console.WriteLine("Enter Password");
+                string pass=Console.ReadLine();
+                Console.WriteLine("ReEnter Password");
+                string repass= Console.ReadLine();
+                Console.WriteLine("Enter Gender");
+                string gen = Console.ReadLine();
+                Console.WriteLine("Enter Faculty");
+                string fac=Console.ReadLine();
+                // writing insert query using prepared statement(paramete)
+                string insquery1 = "insert into tbl_reg values(@id, @username, @password, @repassword,@gender, @faculty)";
+                SqlCommand sc= new SqlCommand(insquery1, conn);
+                sc.Parameters.AddWithValue("id", id);
+                sc.Parameters.AddWithValue("username", uname);
+                sc.Parameters.AddWithValue("password", pass);
+                sc.Parameters.AddWithValue("repassword", repass);
+                sc.Parameters.AddWithValue("gender", gen);
+                sc.Parameters.AddWithValue("faculty", fac);
+                int res=sc.ExecuteNonQuery();
+                if (res > 0)
+                {
+                    Console.WriteLine(res + " Data inserted");
+                }
+                conn.Close() ;
+            }
+            catch (SqlException s)
+            {
+                Console.WriteLine(s);
+            }
+            
+        }
+        public void updateData()
+        {
+            try
+            {
+                string cs = "Data Source=DESKTOP-1GSF0CC;Initial Catalog=db_nccs; Integrated Security=true;Trusted_Connection = True";
+                SqlConnection conn = new SqlConnection(cs);
+                conn.Open();
+                // update query
+                string upquery = "update tbl_reg set username='mandish',faculty='bim' where id=100";
+                SqlCommand sc = new SqlCommand(upquery, conn);
+                int res = sc.ExecuteNonQuery();
+                if (res > 0)
+                {
+                    Console.WriteLine(res + " Data inserted");
+                }
+                conn.Close();
+
+            }
+            catch (SqlException s)
+            {
+                Console.WriteLine(s);
+            }
+        }
+        // update table dynamically
     }
 }
--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -3,7 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Data.SqlClient;//tp connect to SQl and for sql lang
+using System.Data.SqlClient;
+//tp connect to SQl and for sql lang
 //to connect with the eternal datasource like sql server,xml file,sql client
 //packet should be imported
 //steps to connect with database
@@ -16,8 +17,10 @@ namespace DatabaseExmaple
     {
         static void Main(string[] args)
         {
-            CrudeOperation co = new CrudeOperation();
-            co.createTable();
+            CrudOperation co = new CrudOperation();
+            //co.insertData();
+            co.updateData();
+            Console.ReadLine();
         }
     }
 }
